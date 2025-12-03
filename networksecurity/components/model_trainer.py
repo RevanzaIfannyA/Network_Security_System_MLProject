4687da9ba00c13d306732bbfdb9914874483505c
Revanza Ifanny Ardiansyah <revanzaifanny@gmail.com>
2025-12-03 10:58:56 +0700

    pipeline is completed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -24,6 +24,9 @@ from sklearn.ensemble import (
 
 import mlflow
 
+import dagshub
+dagshub.init(repo_owner='RevanzaIfannyA', repo_name='Network_Security_System_MLProject', mlflow=True)
+
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
         try:
@@ -41,7 +44,7 @@ class ModelTrainer:
             mlflow.log_metric("f1_score",f1_score)
             mlflow.log_metric("precision",precision_score)
             mlflow.log_metric("recall_score",recall_score)
-            mlflow.sklearn.log_model(best_model,"model")
+            mlflow.sklearn.log_model(best_model, "model")
                 
     def train_model(self,X_train,y_train,x_test,y_test):
         models = {
